96775591866378ba409b0d609f349e313be6abbc
Ty D'Angelo <tydangelo18@gmail.com>
2020-08-24 15:46:00 -0500

    Made Table Component into multiple components.

--- employee-directory/src/Table.js
+++ employee-directory/src/Table.js
@@ -3,36 +3,53 @@ import React, {Component} from 'react'
 
 // Child Component (Table)
 class Table extends Component {
+    // Load the Child Component (Table) inside render() of the Pare
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Employee</th>
-                        <th>Position</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Fredward</td>
-                        <td>Manager</td>
-                    </tr>
-                    <tr>
-                        <td>DJ</td>
-                        <td>Full Stack Engineer</td>
-                    </tr>
-                    <tr>
-                        <td>Crosby</td>
-                        <td>Systems Admin</td>
-                    </tr>
-                    <tr>
-                        <td>Mina</td>
-                        <td>Accountant</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         )
     }
+}
+
+// Child Component (TableHeader) of Table Component written as a Simple Component
+    // This will be the Header of the Employee Table
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Employee</th>
+                <th>Position</th>
+            </tr>
+        </thead>
+    )
+}
+
+// Child Component (TableBody) of the Table Component written as a Simple Component
+    // This will be the Body of the Employee Table
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Fredward</td>
+                <td>Manager</td>
+            </tr>
+            <tr>
+                <td>DJ</td>
+                <td>Full Stack Engineer</td>
+            </tr>
+            <tr>
+                <td>Crosby</td>
+                <td>Systems Admin</td>
+            </tr>
+            <tr>
+                <td>Mina</td>
+                <td>Accountant</td>
+            </tr>
+        </tbody>
+    )
 }
 
 // Export Child Component (Table) and load it in Parent Component (App)
